6e330330e02b7e12e0f4a2adba9a21ed378fa6e2
Patricio Daza Urqueta <dazaur@gmail.com>
2024-10-24 13:15:48 -0300

    filtro por palabra y edicion de alumnos

--- src/main/java/cl/playground/proyectoalumnos/repository/AlumnoRepository.java
+++ src/main/java/cl/playground/proyectoalumnos/repository/AlumnoRepository.java
@@ -2,6 +2,8 @@ package cl.playground.proyectoalumnos.repository;
 
 import cl.playground.proyectoalumnos.model.Alumno;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +13,11 @@ public interface AlumnoRepository extends JpaRepository<Alumno, Long> {
     List<Alumno> findByNombreContainingIgnoreCase(String nombre);
     List<Alumno> findByEdadBetween(int edadMinima, int edadMaxima);
     List<Alumno> findAlumnoByNombreContainingIgnoreCaseAndEdadBetween(String nombre, int edadMinima, int edadMaxima);
+
+    @Query("SELECT a FROM Alumno a WHERE " +
+            "LOWER(a.nombre) LIKE LOWER(CONCAT('%', :termino, '%')) OR " +
+            "LOWER(a.apellido) LIKE LOWER(CONCAT('%', :termino, '%'))")
+    List<Alumno> findByNombreOrApellido(@Param("termino") String termino);
 }
+
+
